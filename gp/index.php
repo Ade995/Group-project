cd3ab51339247339b68fd98d07be362b3c20e3cf
Ade995 <olumuyiwaadeniyi95@gmail.com>
2019-03-18 01:10:11 +0000

    Add files via upload

---
+++ gp/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>St Joseph</title>
+	<meta name="description" content="">
+	<link rel="stylesheet" href="stylesheet/stylesheet.css">
+</head>
+<body>
+
+	<a href="index.php">		
+		<img src="images/logo.png" alt="St Joseph logo" width="5%" height="5%" align="left"> 
+	</a>
+
+	<br>	
+
+	<header class="group">
+			<h1>St Joseph</h1>		
+	</header>
+
+	<br>
+
+	<nav id="nav_menu">
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Text1</a></li>	
+			<li><a href="">Text2</a></li>	
+		</ul>
+	</nav>
+
+
+
+
+
+
+
+
+
+
+	<footer>			
+				<p>copyright &copy; St Joseph. 2018</p>	
+				
+		</footer>
+
+</body>
+</html>
---
+++ gp/php/organisation.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>St Joseph</title>
+	<meta name="description" content="">
+	<link rel="stylesheet" href="../stylesheet/stylesheet.css">
+</head>
+<body>
+
+	<img src="../images/logo.png" alt="St Joseph logo" width="5%" height="5%" align="left" > <br>
+
+	<header class="group">
+			<h1>St Joseph</h1>		
+	</header>
+
+	<br>
+	
+	<nav id="nav_menu">
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Text1</a></li>	
+			<li><a href="">Text2</a></li>	
+		</ul>
+	</nav>
+
+
+
+
+
+
+
+
+
+
+	<footer>			
+				<p>copyright &copy; St Joseph. 2018</p>	
+				
+		</footer>
+
+</body>
+</html>
